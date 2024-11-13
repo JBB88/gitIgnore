49571547e10e2d37217995cacf384ed643b3cbac
JBB88 <jaybee1118@gmail.com>
2024-11-13 18:59:49 -0500

    gitIgnore v1

---
+++ src/test/java/test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args) {
+         int x = 5;
+         int y =10;
+         int result = x*y;
+        System.out.println(result);
+
+    }
+}
